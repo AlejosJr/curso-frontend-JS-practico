45bc0e3b3f7a324107c5d1993c96baa7245006d7
Jose Alejos <jose.alejos@sybven.com>
2022-10-08 15:13:45 -0400

    Agregando productos al Body

--- main.js
+++ main.js
@@ -6,12 +6,9 @@ const iconCart = document.querySelector(".navbar-shopping-cart");
 const shoppingCart = document.querySelector(".product-detail");
 
 
-
-
 menuEmail.addEventListener("click", toggleDesktopMenu);
 iconHamMenu.addEventListener("click", toggleMobileMenu);
 iconCart.addEventListener("click", toggleIconCart);
-
 
 
 function toggleDesktopMenu() {
@@ -29,3 +26,69 @@ function toggleIconCart() {
     desktopMenu.classList.add("inactive");
     mobileMenu.classList.add("inactive");
 };
+
+const productList = [];
+
+productList.push({
+    name: "Bike",
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt=""'
+});
+productList.push({
+    name: "TV",
+    price: 200,
+    image: 'https://www.sony.com.mx/image/5a01a9fdb85a075ce2d715c1254b08ab?fmt=pjpeg&wid=330&bgcolor=FFFFFF&bgc=FFFFFF alt=""'
+});
+productList.push({
+    name: "Gamer PC",
+    price: 650,
+    image: 'https://www.profesionalreview.com/wp-content/uploads/2017/09/pc-gamer.jpg'
+});
+productList.push({
+    name: "Laptop",
+    price: 550,
+    image: 'https://http2.mlstatic.com/D_NQ_NP_632682-MLV49940965100_052022-W.jpg'
+});
+
+
+
+function renderProductList(listOfProducts){
+    for(product of productList){
+    
+        const productCard = document.createElement('div');
+        productCard.classList.add("product-card");
+    
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+    
+        const productInfo= document.createElement('div');
+        productInfo.classList.add("product-info");
+    
+        const productInfoDiv= document.createElement('div');
+    
+        const productPrice= document.createElement('p');
+        productPrice.innerHTML = "$" + product.price;
+    
+        const productName= document.createElement('p');
+        productName.innerHTML = product.name;
+    
+        const figure = document.createElement("figure");
+    
+        const imgIcon =  document.createElement('img');
+        imgIcon.setAttribute('src', './icons/bt_add_to_cart.svg');   
+    
+        figure.append(imgIcon);
+    
+        productInfoDiv.append(productPrice, productName);
+    
+        productInfo.append(productInfoDiv, figure);
+    
+        productCard.append(img, productInfo)
+    
+        const cardsContainer = document.querySelector(".cards-container");
+        cardsContainer.append(productCard);
+    
+    }
+}
+
+renderProductList(productList);
